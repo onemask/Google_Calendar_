67f034e59a2119a08ab91f59cef0f964f53bc4de
kimsooyeon <onemask@stunitas.com>
2019-01-11 14:27:01 +0900

    안쓰는 함수 정리.

--- app/src/main/java/com/example/onemask/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/onemask/myapplication/MainActivity.kt
@@ -53,25 +53,36 @@ class MainActivity : DaggerAppCompatActivity() {
 
         compositeDisposable = CompositeDisposable()
         button_auth.setOnClickListener {
-            getAuth()
+            isGooglePlayServiceAvailable()
         }
-
     }
 
-    fun getAuth() {
+    private fun isGooglePlayServiceAvailable(): Boolean {
+        googleAccountCredential.selectedAccountName?.let {
+            Toast.makeText(this, "구글 인증이 되었습니다.", Toast.LENGTH_SHORT).show()
+            isgoogle = true
+            changeButton()
+            return true
+        }.let {
+            Toast.makeText(this, "구글 인증을 선택해주세요.", Toast.LENGTH_SHORT).show()
+            isgoogle = false
+            choseAccount()
+            return false
+        }
+    }
 
-        isGooglePlayServiceAvailable()
+    private fun changeButton(){
+        button_auth.visibility = View.INVISIBLE
+        button_month.visibility = View.VISIBLE
+        button_holiday.visibility = View.VISIBLE
 
         button_holiday.setOnClickListener {
             getCalendarList()
         }
-
         button_month.setOnClickListener {
             getEventList(calendarId)
         }
-
     }
-
     private fun getCalendarList() {
         googleCalendarRepository.getCalendarList()
             .observeOn(AndroidSchedulers.mainThread())
@@ -112,111 +123,58 @@ class MainActivity : DaggerAppCompatActivity() {
                 .apply {
                     compositeDisposable.add(this)
                 }
-
-    }
-
-    private fun getResultFromApi() {
-        if (!isGooglePlayServiceAvailable()) {
-            acquireGooglePlayServices()
-        } else {
-            googleAccountCredential.selectedAccountName?.let {
-                //getCalendarList()
-                REQUEST_ACCOUNT = googleAccountCredential.selectedAccountName
-            }.let {
-                //choseAccount()
-            }
-        }
-    }
-
-    private fun isGooglePlayServiceAvailable(): Boolean {
-        googleAccountCredential.selectedAccountName?.let {
-            Toast.makeText(this, "구글 인증이 되었습니다.", Toast.LENGTH_SHORT).show()
-            isgoogle = true
-            changeButton()
-            return true
-        }.let {
-            Toast.makeText(this, "구글 인증을 선택해주세요.", Toast.LENGTH_SHORT).show()
-            isgoogle = false
-            choseAccount()
-            return false
-        }
-    }
-
-    private fun changeButton(){
-        button_auth.visibility = View.INVISIBLE
-        button_month.visibility = View.VISIBLE
-        button_holiday.visibility = View.VISIBLE
     }
 
     @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
     private fun choseAccount() {
-        // GET_ACCOUNTS 권한 O
         if (hasPermissions(this, android.Manifest.permission.GET_ACCOUNTS)) {
             val accountName: String? = getPreferences(Context.MODE_PRIVATE).getString(REQUEST_ACCOUNT, null)
             accountName?.let {
                 googleAccountCredential.selectedAccountName = accountName
-            }.let {
-                startActivityForResult(googleAccountCredential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
-            }
+            } ?: startActivityForResult(googleAccountCredential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
         } else {
-            // 사용자에게 GET_ACCOUNTS 권한을 요구하는 다이얼로그를 보여준다.(주소록 권한 요청함)
-            EasyPermissions.requestPermissions(
-                this,
-                "구글 계정 권한이 필요합니다.",
-                REQUEST_PERMISSION_GET_ACCOUNTS,
-                android.Manifest.permission.GET_ACCOUNTS
-            )
+            requestPermission()
         }
     }
 
-
-    private fun acquireGooglePlayServices() {
-        val availability: GoogleApiAvailability = GoogleApiAvailability.getInstance()
-        val state = availability.isGooglePlayServicesAvailable(this)
-        if (availability.isUserResolvableError(state)) {
-            showErrorDialog(state);
-        }
-    }
-
-    private fun showErrorDialog(state: Int) {
-        val availability: GoogleApiAvailability = GoogleApiAvailability.getInstance()
-        val dialog: Dialog = (availability.getErrorDialog(this, state, REQUEST_CODE_PLAY_SERVICE))
-        dialog.show()
+    private fun requestPermission(){
+        EasyPermissions.requestPermissions(
+            this,
+            "구글 계정 권한이 필요합니다.",
+            REQUEST_PERMISSION_GET_ACCOUNTS,
+            android.Manifest.permission.GET_ACCOUNTS)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        when (requestCode) {
-            REQUEST_CODE_PLAY_SERVICE -> {
-                if (resultCode == Activity.RESULT_OK) {
-                    text_field.text = "구글 플레이 서비스를 설치 해주세요."
-                } else
-                    getResultFromApi()
-            }
-            REQUEST_ACCOUNT_PICKER -> {
-                if (resultCode == Activity.RESULT_OK) {
-                    val accountName: String? = data?.getStringExtra(AccountManager.KEY_ACCOUNT_NAME)
-                    accountName?.let {
-                        getPreferences(Context.MODE_PRIVATE).edit().apply {
-                            putString(REQUEST_ACCOUNT, accountName)
-                            getResultFromApi()
-                            apply()
+            when (requestCode) {
+                 REQUEST_CODE_PLAY_SERVICE -> {
+                     if (resultCode == Activity.RESULT_OK) {
+                         text_field.text = "구글 플레이 서비스를 설치 해주세요."
+                     } else {
+                         return
+                     }
+                 }
+                REQUEST_ACCOUNT_PICKER -> {
+                    if (resultCode == Activity.RESULT_OK) {
+                        val accountName: String? = data?.getStringExtra(AccountManager.KEY_ACCOUNT_NAME)
+                        accountName?.let {
+                            getPreferences(Context.MODE_PRIVATE).edit().apply {
+                                putString(REQUEST_ACCOUNT, accountName)
+                                apply()
+                            }
+                            googleAccountCredential.selectedAccountName = accountName
                         }
-                        googleAccountCredential.selectedAccountName = accountName
                     }
                 }
-            }
-            REQUEST_AUTHORIZATION -> {
-                if (requestCode == Activity.RESULT_OK)
-                    getResultFromApi()
-            }
-            RC_AUTH_PERMISSION -> {
-                Toast.makeText(this, "구글 인증이 필요합니다.", Toast.LENGTH_SHORT).show()
-                getResultFromApi()
-            }
+                RC_AUTH_PERMISSION -> {
+                    Toast.makeText(this, "구글 인증이 필요합니다.", Toast.LENGTH_SHORT).show()
+                    isGooglePlayServiceAvailable()
+                }
 
+            }
         }
     }
-}
+
 
 
 
