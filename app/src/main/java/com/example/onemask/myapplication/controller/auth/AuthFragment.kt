394e1d14f8ba86391137301aa6a5426402f08b25
kimsooyeon <onemask@stunitas.com>
2019-01-16 15:29:32 +0900

    navigation error commit

--- app/src/main/java/com/example/onemask/myapplication/controller/auth/AuthFragment.kt
+++ app/src/main/java/com/example/onemask/myapplication/controller/auth/AuthFragment.kt
@@ -2,17 +2,13 @@ package com.example.onemask.myapplication.controller.auth
 
 import android.accounts.AccountManager
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
-
 import com.example.onemask.myapplication.R
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
 import dagger.android.support.DaggerFragment
@@ -80,8 +76,5 @@ class AuthFragment : DaggerFragment() {
             findNavController().navigate(this)
         }
     }
-
-
-
 
 }
